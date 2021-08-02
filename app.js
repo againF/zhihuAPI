7d8b2907defc43d5356355478b21c6affa5452fa
Alan <wyffcld@163.com>
2021-08-02 17:15:14 +0800

    增删改查最佳实践

--- app.js
+++ app.js
@@ -16,16 +16,26 @@ router.get("/",login, (ctx, next) => {
     
     ctx.body = `index ctx:`
 })
+// 获取用户，返回数组
 userRouter.get("/", (ctx, next) => {
-    ctx.body = "get user"
+    ctx.body = [{name: '李雷'},{name: '韩梅梅'}];
 })
+// 新建用户，返回新建的用户
 userRouter.post("/", (ctx, next) => {
-    ctx.body = "post user"
+    ctx.body = {name: '李雷'}
 })
+// 获取特定用户，返回特定的用户
 userRouter.get("/:id", (ctx, next) => {
-    ctx.body = `get user id:${ctx.params.id}`
+    ctx.body = {name: '李雷'}
 })
-
+// 修改返回修改后的项
+userRouter.put("/:id", (ctx, next) => {
+    ctx.body = {name: '李雷2'}
+})
+// 删除，返回状态码204，代表成功了，但是没有返回值
+userRouter.delete("/:id", (ctx, next) => {
+    ctx.status = 204
+})
 app
   .use(router.routes())
   .use(userRouter.routes())
