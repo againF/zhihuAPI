e9dbda2598c14fc5d4cbcae2229f7ccd2e6b28b4
Alan <wyffcld@163.com>
2021-08-18 22:21:52 +0800

    ci

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 const Koa = require("koa");
+const bodyparser = require("koa-bodyparser");
 const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
+const hostName = '127.0.0.1'; //IP
+const port = 3000; //端口
+let publishStatus = ""; // 网站发布状态
+// 处理跨域
+app.use(async (ctx, next) => {
+    ctx.set("Access-Control-Allow-Origin", "*")
+    await next()
+  })
+// 用户
 const userRouter = new Router({
     prefix: "/user"
+})
+// 轮询
+const askRouter = new Router({
+    prefix: "/ask"
+})
+
+askRouter.get("/publish/status",(ctx) => {
+    ctx.body = publishStatus
+}) 
+
+router.post("/build", (ctx) => {
+    publishStatus = "开始发布"
+    ctx.body="开始发布"
+    setTimeout(() => {
+        publishStatus = "进度30%"
+    }, 3000);
+    setTimeout(() => {
+        publishStatus = "进度60%"
+    }, 6000);
+    setTimeout(() => {
+        publishStatus = "进度90%"
+    }, 9000);
+    setTimeout(() => {
+        publishStatus = "进度100%"
+    }, 12000);
 })
 const login = (ctx, next) => {
     // ctx.throw(405)
@@ -18,10 +53,13 @@ router.get("/",login, (ctx, next) => {
 })
 // 获取用户，返回数组
 userRouter.get("/", (ctx, next) => {
-    ctx.body = [{name: '李雷'},{name: '韩梅梅'}];
+    ctx.body = [{name: '李雷'},{name: '韩梅梅1'}];
 })
 // 新建用户，返回新建的用户
 userRouter.post("/", (ctx, next) => {
+    let body = ctx.request.body;
+    console.log('body:',body);
+    
     ctx.body = {name: '李雷'}
 })
 // 获取特定用户，返回特定的用户
@@ -37,9 +75,14 @@ userRouter.delete("/:id", (ctx, next) => {
     ctx.status = 204
 })
 app
+.use(bodyparser())
   .use(router.routes())
   .use(userRouter.routes())
+  .use(askRouter.routes())
   .use(userRouter.allowedMethods()) // 响应options方法；自动相应返回405,501状态码
-  .use(router.allowedMethods()); // 响应options方法；自动相应返回405,501状态码
-app.listen(3000);
-console.log('app listen on 3000');
+  .use(router.allowedMethods()) // 响应options方法；自动相应返回405,501状态码
+  .use(askRouter.allowedMethods()) // 响应options方法；自动相应返回405,501状态码
+app.listen(port,hostName,() => {
+    console.log(`服务运行在http://${hostName}:${port}`);
+});
+
